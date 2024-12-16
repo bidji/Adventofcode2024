849d42e8c4c0a2d4c8921bae352b16ff8b77ff61
David Morineau <d.morineau@artis.fr>
2024-12-17 00:05:29 +0100

    code cleaning

--- day13/solve.py
+++ day13/solve.py
@@ -26,12 +26,7 @@ def count_needed_tokens(filename: str) -> int:
     needed_tokens = 0
     nb_prizes = 0
     for machine in machines:
-        if (machine['A X+'] + machine['B X+']) * 100 < machine['Prize X'] or (machine['A Y+'] + machine['B Y+'])* 100 < machine['Prize Y']:
-            # not reachable, skip
-            continue
-        
         nb_tokens = 0
-        result = None
         max_a = min(int(machine['Prize X']/machine['A X+']), int(machine['Prize Y']/machine['A Y+']), 100)
         for a in range(0, max_a + 1):
             if (machine['Prize X'] - (a * machine['A X+'])) % machine['B X+'] == 0 and (machine['Prize Y'] - (a * machine['A Y+'])) % machine['B Y+'] == 0:
@@ -40,10 +35,7 @@ def count_needed_tokens(filename: str) -> int:
                 if b >= 0 and b <= 100 and a * machine['A Y+'] + b * machine['B Y+'] == machine['Prize Y']:
                     if nb_tokens == 0:
                         nb_tokens = a * 3 + b
-                        result = f"a={a} b={b} => {a * 3 + b} tokens"
                     else:
-                        if nb_tokens > a * 3 + b:
-                            result = f"a={a} b={b} => {a * 3 + b} tokens"
                         nb_tokens = min(a * 3 + b, nb_tokens)
         
         if nb_tokens > 0:
