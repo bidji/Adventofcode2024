653e9e262f32cc5e12c672888a0766484743eea7
David Morineau <d.morineau@artis.fr>
2024-12-19 15:07:42 +0100

    day 12 part 1 finished

--- day12/solve.py
+++ day12/solve.py
@@ -5,30 +5,60 @@ def load(filename: str):
             grid.append(line.strip())
     return grid
 
-def get_regions(grid):
-    # regions initialisation
+
+def next_region(areas: dict[int, int]) -> int:
+    if len(areas) == 0:
+        return 1
+    else:
+        return max(areas.keys()) + 1
+
+def set_region(grid: list[list[int]], regions: list[list[int]],
+               areas: dict[int, int], j: int, i: int):
+    # check if region already set
+    if regions[j][i] > 0:
+        return
+    
+    plot = grid[j][i]
+    
+    # check if there is a plot connected with a region already set
+    region = 0
+    if j > 0 and grid[j-1][i] == plot and regions[j-1][i] > 0:
+        region = regions[j-1][i]
+    if i > 0 and grid[j][i-1] == plot and regions[j][i-1] > 0:
+        region = regions[j][i-1]
+    if j < len(grid) - 1 and grid[j+1][i] == plot and regions[j+1][i] > 0:
+        region = regions[j+1][i]
+    if i < len(grid[j]) - 1 and grid[j][i+1] == plot and regions[j][i+1] > 0:
+        region = regions[j][i+1]
+        
+    if region > 0:
+        areas[region] += 1
+    else:
+        region = next_region(areas)
+        areas[region] = 1
+    regions[j][i] = region
+    
+    # set region for connected plots without a region set
+    if j > 0 and grid[j-1][i] == plot and regions[j-1][i] == 0:
+        set_region(grid, regions, areas, j-1, i)
+    if i > 0 and grid[j][i-1] == plot and regions[j][i-1] == 0:
+        set_region(grid, regions, areas, j, i-1)
+    if j < len(grid) - 1 and grid[j+1][i] == plot and regions[j+1][i] == 0:
+        set_region(grid, regions, areas, j+1, i)
+    if i < len(grid[j]) - 1 and grid[j][i+1] == plot and regions[j][i+1] == 0:
+        set_region(grid, regions, areas, j, i+1)
+
+def get_regions_areas(grid):
+    # regions and mapped initialisation
     regions = []
     for row in grid:
         regions.append([0] * len(row))
     
-    num_region = 1
+    areas = dict()
     for j in range(0, len(grid)):
         for i in range(0, len(grid[j])):
-            if j > 0 and grid[j][i] == grid[j-1][i]:
-                regions[j][i] = regions[j-1][i]
-            elif i > 0 and grid[j][i] == grid[j][i-1]:
-                regions[j][i] = regions[j][i-1]
-            else:
-                regions[j][i] = num_region
-                num_region += 1
-            
-    areas = dict()
-    for row in regions:
-        for plot in set(row):
-            if plot in areas:
-                areas[plot] = areas[plot] + row.count(plot)
-            else:
-                areas[plot] = row.count(plot)
+            set_region(grid, regions, areas, j, i)
+    
     return regions, areas
 
 def get_price(regions, areas, j: int, i: int) -> int:
@@ -60,8 +90,7 @@ def get_prices(filename: str):
     grid = load(filename)
     
     # define regions in mapped
-    regions, areas = get_regions(grid)
-    display(regions, areas)
+    regions, areas = get_regions_areas(grid)
     
     # sum prices
     prices = 0
@@ -69,7 +98,7 @@ def get_prices(filename: str):
         row_prices = []
         for i in range(0, len(grid[j])):
             row_prices.append(get_price(regions, areas, j, i))
-        print(" ".join([str(p).zfill(2) for p in row_prices]))
+        # print(" ".join([str(p).zfill(2) for p in row_prices]))
         prices += sum(row_prices)
     return prices
 
@@ -77,3 +106,4 @@ print("first part:")
 print(get_prices('day12/sample1'))
 print(get_prices('day12/sample2'))
 print(get_prices('day12/sample3'))
+print(get_prices('day12/input'))
